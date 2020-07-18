c93dc43ed26f545f5c931c0a3bd19c90f37457c0
luuquan09 <63362688+luuquan09@users.noreply.github.com>
2020-07-18 10:09:30 +0700

    first

---
+++ animation.js
@@ -0,0 +1,10 @@
+$(".txt input").on("focus",function(){
+    $(this).addClass("focus");
+  });
+
+  $(".txt input").on("blur",function(){
+    if($(this).val() == "")
+    $(this).removeClass("focus");
+  });
+
+
